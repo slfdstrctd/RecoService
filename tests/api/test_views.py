86346e632a8b5809abba876a6a635cdfc4c2a288
Artem <artichek123@gmail.com>
2023-11-22 18:16:49 +0300

    Add token from env to tests, change jobs name in config

--- tests/api/test_views.py
+++ tests/api/test_views.py
@@ -1,3 +1,4 @@
+import os
 from http import HTTPStatus
 
 from starlette.testclient import TestClient
@@ -5,6 +6,8 @@ from starlette.testclient import TestClient
 from service.settings import ServiceConfig
 
 GET_RECO_PATH = "/reco/{model_name}/{user_id}"
+
+TOKEN = os.getenv("TOKEN")
 
 
 def test_health(
@@ -23,8 +26,7 @@ def test_get_reco_success(
     path = GET_RECO_PATH.format(model_name="some_model", user_id=user_id)
 
     with client:
-        response = client.get(path,
-                              headers={"Authorization": "Bearer topsecret"})
+        response = client.get(path, headers={"Authorization": f"Bearer {TOKEN}"})
     assert response.status_code == HTTPStatus.OK
     response_json = response.json()
     assert response_json["user_id"] == user_id
@@ -35,12 +37,11 @@ def test_get_reco_success(
 def test_get_reco_for_unknown_user(
     client: TestClient,
 ) -> None:
-    user_id = 10 ** 10
+    user_id = 10**10
     path = GET_RECO_PATH.format(model_name="some_model", user_id=user_id)
 
     with client:
-        response = client.get(path,
-                              headers={"Authorization": "Bearer topsecret"})
+        response = client.get(path, headers={"Authorization": f"Bearer {TOKEN}"})
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json()["errors"][0]["error_key"] == "user_not_found"
 
@@ -52,8 +53,7 @@ def test_get_reco_for_unknown_model(
     path = GET_RECO_PATH.format(model_name="bad_model", user_id=user_id)
 
     with client:
-        response = client.get(path,
-                              headers={"Authorization": "Bearer topsecret"})
+        response = client.get(path, headers={"Authorization": f"Bearer {TOKEN}"})
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json()["errors"][0]["error_key"] == "model_not_found"
 
@@ -65,7 +65,6 @@ def test_get_reco_unknown_token(
     path = GET_RECO_PATH.format(model_name="some_model", user_id=user_id)
 
     with client:
-        response = client.get(path,
-                              headers={"Authorization": "Bearer asdasdasd"})
+        response = client.get(path, headers={"Authorization": "Bearer asdasdasd"})
     assert response.status_code == HTTPStatus.UNAUTHORIZED
     assert response.json()["errors"][0]["error_key"] == "token_error"
