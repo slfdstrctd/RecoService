3df5670811d609841deee9cd6f153b540c9bc721
Artem <artichek123@gmail.com>
2023-11-29 16:59:48 +0300

    Add homework 3

--- service/api/views.py
+++ service/api/views.py
@@ -1,15 +1,23 @@
+import pickle
 from typing import List
 
 from fastapi import APIRouter, FastAPI, Request
 from pydantic import BaseModel
 
-from service.api.exceptions import UserNotFoundError
+from models.userknn import UserKnn
+from service.api.exceptions import ModelNotFoundError, UserNotFoundError
 from service.log import app_logger
 
 
 class RecoResponse(BaseModel):
     user_id: int
     items: List[int]
+
+
+def load_model_from_pickle(file_path) -> UserKnn:
+    with open(file_path, "rb") as f:
+        model = pickle.load(f)
+    return model
 
 
 router = APIRouter()
@@ -35,13 +43,18 @@ async def get_reco(
 ) -> RecoResponse:
     app_logger.info(f"Request for model: {model_name}, user_id: {user_id}")
 
-    # Write your code here
-
     if user_id > 10**9:
         raise UserNotFoundError(error_message=f"User {user_id} not found")
 
     k_recs = request.app.state.k_recs
-    reco = list(range(k_recs))
+
+    if model_name == "some_model":
+        reco = list(range(k_recs))
+    elif model_name == "userknn":
+        uknn = load_model_from_pickle("saved_models/userknn2.pkl")
+        reco = uknn.recommend(user_id=int(user_id), N_recs=10)
+    else:
+        raise ModelNotFoundError(error_message=f"Model {model_name} not found")
     return RecoResponse(user_id=user_id, items=reco)
 
 
