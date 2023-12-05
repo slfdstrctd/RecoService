7c43a10fd6207b4345eb008c42746baff964304f
Artem <artichek123@gmail.com>
2023-12-05 19:15:27 +0300

    Code refactoring

--- service/api/views.py
+++ service/api/views.py
@@ -32,32 +32,26 @@ async def health() -> str:
     responses={
         404: {
             "description": "Not found",
-            "content": {"application/json": {
-                "example": {"detail": "Model or user not found"}}
-            },
+            "content": {"application/json": {"example": {"detail": "Model or user not found"}}},
         },
         401: {
             "description": "Not authorized",
-            "content": {"application/json": {
-                "example": {"detail": "Authorization failed"}}},
+            "content": {"application/json": {"example": {"detail": "Authorization failed"}}},
         },
     },
 )
 async def get_reco(
-    request: Request,
-    model_name: str,
-    user_id: int,
-    token: HTTPAuthorizationCredentials = Depends(bearer)
+    request: Request, model_name: str, user_id: int, token: HTTPAuthorizationCredentials = Depends(bearer)
 ) -> RecoResponse:
     app_logger.info(f"Request for model: {model_name}, user_id: {user_id}")
 
     if request.app.state.token != token.credentials:
         raise UnauthorizedUserError()
 
-    if user_id > 10 ** 9:
+    if user_id > 10**9:
         raise UserNotFoundError(error_message=f"User {user_id} not found")
 
-    if model_name != 'some_model':
+    if model_name != "some_model":
         raise ModelNotFoundError(error_message=f"Model {model_name} not found")
 
     k_recs = request.app.state.k_recs
